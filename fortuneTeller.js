3c33081b574fa2529877f08d952b70a2ed781841
ZolsKe <zoltan.the.kepes@gmail.com>
2021-06-09 18:00:57 +0100

    fortune teller text added, with 3 random parts

--- fortuneTeller.js
+++ fortuneTeller.js
@@ -1,5 +1,5 @@
 // 1st random component, when array
-const when = ['in 1078 minutes', 'in 62,5 hours', 'tomorrow', 'this weekend', 'next week', 'next month', 'next year'];
+const when = ['In 1078 minutes', 'In 62,5 hours', 'Tomorrow', 'This weekend', 'Next week', 'Next month', 'Next year','In your next lifetime'];
 
 // 2nd random component, what array
 const what = ['you will win the lottery', 'you will meet the love of your life', 'you will stuck in the lift', 'your fridge will break down', 'it will start to snow', 'you will get an invitation from the queen', 'your neighbour will ask you for sugar', 'you will run out of toilet paper', 'there will be a blackout'];
@@ -11,4 +11,5 @@ const ifThan = ['if you don`t shower', 'if you eat a lemon every day', 'if you p
 const pickRandomElement = array => 
     array[Math.floor(Math.random() * array.length)]
 
-console.log(pickRandomElement(when));
+    
+console.log(`I am Zoltran, your fortune teller.\nStrange things will happen in your life.\n\n${pickRandomElement(when)} ${pickRandomElement(what)}. But only ${pickRandomElement(ifThan)}.`);
